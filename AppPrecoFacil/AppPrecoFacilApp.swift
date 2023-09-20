4c0de30c1177070ae6873369c49d384ad26a8dba
NicolasArvani <nicolas.arvani@gmail.com>
2023-09-19 21:31:28 -0300

    first commit

---
+++ AppPrecoFacil/AppPrecoFacilApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppPrecoFacilApp.swift
+//  AppPrecoFacil
+//
+//  Created by user on 19/09/23.
+//
+
+import SwiftUI
+
+@main
+struct AppPrecoFacilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
