7eeae0a0b9df26acfa56ae797d50a3f768ce475e
NicolasArvani <nicolas.arvani@gmail.com>
2023-10-21 23:54:22 -0300

    novas atualizacoes

--- AppPrecoFacil/TelasPrincipais/TelaHome.swift
+++ AppPrecoFacil/TelasPrincipais/TelaHome.swift
@@ -24,20 +24,49 @@ struct TelaHome: View {
             .background(Color.gray.opacity(0.3))
             .cornerRadius(30)
             .border(.gray.opacity(0.05))
-           // .padding(.bottom, 10)
+            // .padding(.bottom, 10)
             Spacer()
             
-            Text("Tela Home")
-             NavigationLink("Ir para perfil", destination: TelaPerfil())
-            Spacer()
+            ZStack(alignment: .topTrailing) {
+                ZStack(alignment: .bottom) {
+                    Image("profile")
+                        .resizable()
+                        .cornerRadius(20)
+                        .frame(width: 130, height: 200)
+                       //.scaledToFit()
+                    
+                    VStack(alignment: .leading) {
+                        Text("rick")
+                            .bold()
+                        
+                        //  Text("\(product.price)$")
+                        //    .font(.caption)
+                    }
+                    .padding()
+                    .frame(width: 180, alignment: .leading)
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(20)
+                }
+                .frame(width: 180, height: 250)
+              // .shadow(radius: 3)
+                
+                Button {
+                    // cartManager.addToCart(product: product)
+                } label: {
+                    Image(systemName: "plus")
+                        .padding(10)
+                        .foregroundColor(.black)
+                        .background(.white)
+                        .cornerRadius(50)
+                        .padding()
+                }
+            }
         }
-
-        
     }
-}
-
-struct TelaHome_Previews: PreviewProvider {
-    static var previews: some View {
-        TelaHome()
+    
+    struct TelaHome_Previews: PreviewProvider {
+        static var previews: some View {
+            TelaHome()
+        }
     }
 }
